91bfee81d9b14f6897beae2f0071cfc8ad2fe605
prince-rockwallet <pprince@mnee.io>
2025-10-23 09:20:07 +0530

    chore: integration test for parser and test address in env

--- balance_test.go
+++ balance_test.go
@@ -16,6 +16,11 @@ func TestGetBalances_Integration(t *testing.T) {
 		t.Skip("Skipping integration test: MNEE_API_KEY environment variable not set")
 	}
 
+	testAddress := os.Getenv("TEST_ADDRESS")
+	if testAddress == "" {
+		t.Skip("Skipping integration test: TEST_ADDRESS environment variable not set")
+	}
+
 	m, err := NewMneeInstance(EnvSandbox, apiKey)
 	assertions.Nil(err, "NewMneeInstance should not return an error")
 	assertions.NotNil(m, "MneeInstance should not be nil")
@@ -28,9 +33,8 @@ func TestGetBalances_Integration(t *testing.T) {
 	assertions.NotNil(config.FeeAddress, "FeeAddress in config is nil")
 
 	feeAddress := *config.FeeAddress
-	randomAddress := "1KtQkZKHPwmdy7WtHWXz8PyUyrVy11y4ED"
 
-	addressesToTest := []string{feeAddress, randomAddress}
+	addressesToTest := []string{feeAddress, testAddress}
 
 	balances, err := m.GetBalances(context.Background(), addressesToTest)
 
@@ -38,19 +42,19 @@ func TestGetBalances_Integration(t *testing.T) {
 	assertions.NotNil(balances, "Balances response should not be nil")
 	assertions.Len(balances, 2, "Should get 2 balance results back")
 
-	var foundFeeAddress, foundRandomAddress bool
+	var foundFeeAddress, foundTestAddress bool
 	for _, balance := range balances {
 		switch *balance.Address {
 		case feeAddress:
 			assertions.Greater(balance.Amt, float64(0), "Fee address should have a balance")
 			foundFeeAddress = true
-		case randomAddress:
-			assertions.Equal(balance.Amt, float64(1000000), "Random address should have 0 balance")
-			foundRandomAddress = true
+		case testAddress:
+			assertions.Equal(balance.Amt, float64(1000000), "Test address should have 0 balance")
+			foundTestAddress = true
 		}
 	}
 	assertions.True(foundFeeAddress, "Did not find balance for feeAddress")
-	assertions.True(foundRandomAddress, "Did not find balance for randomAddress")
+	assertions.True(foundTestAddress, "Did not find balance for testAddress")
 
 	t.Logf("Successfully fetched balances for %d addresses", len(balances))
 
